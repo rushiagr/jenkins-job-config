1b4424606bfb0500670fecbe00d0e92f68f86594
Rushi Agrawal <rushi.agr@gmail.com>
2013-12-04 18:07:07 +0530

    Add provision for pushing changes from upstream to jiocloud/devmaster

--- cinder-jiocloud.sh
+++ cinder-jiocloud.sh
@@ -1,11 +1,23 @@
 if [ "$GIT_BRANCH" = "origin/master" ]
 then
+  # Push rebased changes to jiocloud/master, and merged 
+  # changes to jiocloud/devmaster
+
   git checkout -b jiocloud_master jiocloud/master;
   git rebase origin/master;
   tox -e py27;
   git push jiocloud jiocloud_master:master --force;
-  git checkout master;
+
+  # If rebase succeeds, then merge too will!
+  # NOTE!: merge may not, as jiocloud/devmaster might have 
+  # changed, but that is user's fault!
+  git checkout -b jiocloud_devmaster jiocloud/devmaster;
   git branch -D jiocloud_master;
+  git merge origin/master;
+  git push jiocloud jiocloud_devmaster:devmaster;
+
+  git checkout master;
+  git branch -D jiocloud_devmaster;
 elif [ "$GIT_BRANCH" = "jiocloud/manualmaster" ]
 then
   git checkout -b jiocloud_manualmaster;
